b0a1799d482d7ab6f33deefccb03554f5845b02e
Daniel Choi <dhchoi@gmail.com>
2011-02-18 08:38:13 -0500

    making this a pure ruby project for now

--- vnews-clj/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject vnews-clj "1.0.0-SNAPSHOT"
-  :description "A Vim newsreader"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
-
-; project requires jdom-1.1.jar  rome-1.0.jar in the lib directory
--- vnews-clj/test/vnews_clj/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns vnews-clj.test.core
-  (:use [vnews-clj.core] :reload)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
