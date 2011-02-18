3666f2bd4f20f990c7bfd6474533b99e3fc64136
Daniel Choi <dhchoi@gmail.com>
2011-02-17 23:33:44 -0500

    integration with feedparser-clj

--- vnews-clj/project.clj
+++ vnews-clj/project.clj
@@ -1,4 +1,6 @@
 (defproject vnews-clj "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "A Vim newsreader"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
+
+; project requires jdom-1.1.jar  rome-1.0.jar in the lib directory
